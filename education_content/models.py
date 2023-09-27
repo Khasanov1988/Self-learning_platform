5a54e36453309eb9eab852db4a9207543f36f1c8
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-09-27 21:34:56 +0200

    Boostrap css and js added. educational_content models added.

--- education_content/models.py
+++ education_content/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from config import settings
+
+
+class Chapter(models.Model):
+    """
+    Chapter class
+    """
+    title = models.CharField(max_length=100, verbose_name='Title')
+    description = models.CharField(max_length=2000, verbose_name='Description')
+    preview = models.ImageField(null=True, blank=True, verbose_name='Preview')
+    made_date = models.DateField(default=timezone.now().date(), verbose_name='Creation date')
+    last_update = models.DateField(default=None, null=True, blank=True, verbose_name='Last update date')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+                              verbose_name='Owner')
+    is_published = models.BooleanField(default=True, verbose_name='Publication Status')
+
+    def __str__(self):
+        return f'Chapter - {self.title}'
+
+    class Meta:
+        verbose_name = 'Chapter'
+        verbose_name_plural = 'Chapters'
+
+
+class Material(models.Model):
+    """
+    Educational material class
+    """
+    topic = models.CharField(max_length=100, verbose_name='Title')
+    description = models.CharField(max_length=300, null=True, blank=True, verbose_name='Description')
+    text = models.TextField(verbose_name='Text')
+    preview = models.ImageField(null=True, blank=True, verbose_name='Preview')
+    made_date = models.DateField(default=timezone.now().date(), verbose_name='Creation date')
+    last_update = models.DateField(default=None, null=True, blank=True, verbose_name='Last update date')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+                              verbose_name='Owner')
+    is_published = models.BooleanField(default=True, verbose_name='Publication Status')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='Number of Views')
+
+    def __str__(self):
+        return f'Material - {self.topic}'
+
+    class Meta:
+        verbose_name = 'Material'
+        verbose_name_plural = 'Materials'
+
+
+class MaterialPhotos(models.Model):
+    """
+    Material figures
+    """
+    signature = models.CharField(max_length=100, default=None, null=True, blank=True, verbose_name='Signature')
+    figure = models.ImageField(verbose_name='Figure')
+    material = models.ForeignKey('education_content.Material', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Figure - {self.signature}'
+
+    class Meta:
+        verbose_name = 'Figure'
+        verbose_name_plural = 'Figures'
