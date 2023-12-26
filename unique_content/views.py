ac0c9faa519283f7e4c0c0c5d57fefa236c296f2
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-12-26 22:23:04 +0100

    VMM functions transferred to static, Last activity time for user model was added

--- unique_content/views.py
+++ unique_content/views.py
@@ -2,12 +2,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 from unique_content.models import FigureThinSection, FigureFromP3din
+from users.services import update_last_activity
 
 
 class FigureThinSectionDetailView(LoginRequiredMixin, DetailView):
     model = FigureThinSection
 
     def get_context_data(self, **kwargs):
+        update_last_activity(self.request.user)
         context_data = super().get_context_data()
         labels_list = self.object.label_set.all().select_related('mineral')
         context_data['labels_list'] = labels_list
@@ -20,6 +22,11 @@ class FigureThinSectionListView(LoginRequiredMixin, ListView):
 
 class FigureFromP3dinDetailView(LoginRequiredMixin, DetailView):
     model = FigureFromP3din
+
+    def get_context_data(self, **kwargs):
+        update_last_activity(self.request.user)
+        context_data = super().get_context_data()
+        return context_data
 
 
 class FigureFromP3dinListView(LoginRequiredMixin, ListView):
