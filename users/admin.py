76d4e3cabe19721a99d11f800be8084aa7804e00
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-12-28 15:39:51 +0100

    Ordering by lat activity was added in User admin panel

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'last_name', 'last_activity',)
-    ordering = ('last_activity',)
+    ordering = ('email', 'first_name', 'last_name', 'last_activity',)
 
 
 admin.site.register(User, UserAdmin)
